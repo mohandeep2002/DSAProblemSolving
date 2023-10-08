3135549cbf750f86be9fbc05971e4c87605dd156
Mohandeep Atmuri <mohandeep2002@gmail.com>
2023-10-09 00:00:52 +0530

    .gitignore added

---
+++ SmallestNumber.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Input");
+        int n = scn.nextInt();
+        System.out.println(solution(n));
+    }
+
+    public static int solution(int n) {
+        String ans = "";
+        for (int div = 9; div >= 2; div--) {
+            while (n % div == 0) {
+                n = n / div;
+                ans = div + ans;
+            }
+        }
+        if (n != 1)
+            return -1;
+        return Integer.parseInt(ans);
+    }
+}
